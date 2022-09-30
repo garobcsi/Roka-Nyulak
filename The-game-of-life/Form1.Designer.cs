67537738fc043c5c393f98f7f91dfd45fc57ffe1
garobcsi <gasparrobert0908@gmail.com>
2022-09-30 14:31:32 +0200

    Forms test added

--- The-game-of-life/Form1.Designer.cs
+++ The-game-of-life/Form1.Designer.cs
@@ -29,13 +29,88 @@ namespace The_game_of_life
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbGrid = new System.Windows.Forms.PictureBox();
+            this.btRun = new System.Windows.Forms.Button();
+            this.btStep = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.btDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGrid
+            // 
+            this.pbGrid.Location = new System.Drawing.Point(0, 85);
+            this.pbGrid.Name = "pbGrid";
+            this.pbGrid.Size = new System.Drawing.Size(1279, 591);
+            this.pbGrid.TabIndex = 0;
+            this.pbGrid.TabStop = false;
+            // 
+            // btRun
+            // 
+            this.btRun.BackColor = System.Drawing.Color.Green;
+            this.btRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btRun.Location = new System.Drawing.Point(12, 12);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 43);
+            this.btRun.TabIndex = 1;
+            this.btRun.Text = "START";
+            this.btRun.UseVisualStyleBackColor = false;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // btStep
+            // 
+            this.btStep.Location = new System.Drawing.Point(93, 12);
+            this.btStep.Name = "btStep";
+            this.btStep.Size = new System.Drawing.Size(75, 43);
+            this.btStep.TabIndex = 2;
+            this.btStep.Text = "Step";
+            this.btStep.UseVisualStyleBackColor = true;
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(174, 12);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(75, 43);
+            this.btReset.TabIndex = 3;
+            this.btReset.Text = "Reset";
+            this.btReset.UseVisualStyleBackColor = true;
+            // 
+            // btDraw
+            // 
+            this.btDraw.Location = new System.Drawing.Point(255, 12);
+            this.btDraw.Name = "btDraw";
+            this.btDraw.Size = new System.Drawing.Size(75, 43);
+            this.btDraw.TabIndex = 4;
+            this.btDraw.Text = "Draw";
+            this.btDraw.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1279, 676);
+            this.Controls.Add(this.btDraw);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btStep);
+            this.Controls.Add(this.btRun);
+            this.Controls.Add(this.pbGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbGrid;
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.Button btStep;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Button btDraw;
     }
 }
 
--- The-game-of-life/Form1.cs
+++ The-game-of-life/Form1.cs
@@ -16,5 +16,48 @@ namespace The_game_of_life
         {
             InitializeComponent();
         }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TestPictureBox();
+        }
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+            btRun.BackColor = btRun.BackColor == Color.Green ? Color.Red : Color.Green;
+            btRun.Text = btRun.Text == "START" ? "STOP" : "START";
+        }
+
+        void TestPictureBox()
+        {
+            #region simple test
+            //width 1279 (width) by 15 seps w/ one plus added for gap
+            //width 591 (height) by 15 seps w/ one plus added for gap
+            Bitmap bmp = new Bitmap(pbGrid.Width, pbGrid.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            SolidBrush sb = new SolidBrush(Color.White);
+            g.Clear(Color.Black);
+            for (int i = 0; i < pbGrid.Width; i += 15)
+            {
+                for (int j = 0; j < pbGrid.Height; j += 15)
+                {
+
+                    if (i >= pbGrid.Width - 15 && j >= pbGrid.Height - 15) // test last pice -15 from height and width
+                    {
+                        SolidBrush sb1 = new SolidBrush(Color.OrangeRed);
+                        g.FillRectangle(sb1, new Rectangle(i, j, 15, 15));
+                    }
+                    else
+                    {
+                        g.FillRectangle(sb, new Rectangle(i, j, 15, 15));
+
+                    }
+                    j++; // separator
+                }
+                i++; // separator
+            }
+            pbGrid.Image = (Bitmap)bmp.Clone();
+            #endregion
+        }
     }
 }
