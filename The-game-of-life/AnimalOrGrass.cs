e915be657df0bdcffe6e15a97ee764104d1d9ee7
kozpenz <botond.babik.neu@gmail.com>
2022-10-08 17:39:43 +0200

    AnimalOrGrass Class

---
+++ The-game-of-life/AnimalOrGrass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace The_game_of_life
+{
+    class AnimalOrGrass
+    {
+        public bool DeadOrAlive { get; set; }
+        public Point Cords { get; set; }
+        public enum Animals
+        {
+            Fox,
+            Bunny
+        }
+        public enum Grass
+        {
+            GrassStart,
+            GrassMid,
+            GrassEnd
+        }
+        public bool isAnimal { get; set; } //ha hamis akkor fű
+        public byte Hunger { get; set; }
+    }
+}
