f94296ca99ca8a5b62503e2439aecd66c217b07d
kozpenz <botond.babik.neu@gmail.com>
2022-10-09 14:21:27 +0200

    NextStep not finished

---
+++ The-game-of-life/NextStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_game_of_life
+{
+    class NextStep
+    {
+        Animal an;
+        Grass gr;
+
+        void Next()
+        {
+
+        }
+
+        void Continouosnext()
+        {
+
+        }
+    }
+}
