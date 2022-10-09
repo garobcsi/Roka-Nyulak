a4bc5d05db5a8a55f53d8cf767934481c8059d8d
Alex1023456789 <nemesalex54@gmail.com>
2022-10-09 14:04:34 +0200

    Grass class

---
+++ The-game-of-life/Grass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace The_game_of_life
+{
+    class Grass
+    {
+        public int[,] GrassMatrix { get; set; }
+        enum GrassType
+        {
+            GrassStart,
+            GrassMid,
+            GrassEnd
+        }
+        public Point GrassCords { get; set; } = new Point();
+    }
+}
